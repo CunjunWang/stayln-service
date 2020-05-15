1a19e956fcbc7cca852666d133ccc207374be4e6
Willincia1124 <jerrynirvana@outlook.com>
2020-05-15 14:20:31 -0400

    for deploy

--- stayln/stayln/settings.py
+++ stayln/stayln/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'staylndb',
         'USER': 'postgres',
-        'PASSWORD': '65261599Duck',
+        'PASSWORD': '86901376',
         'HOST': 'localhost'
     }
 }
